61d1153549a46e4ace0773215595ef4249bf1aee
ParubovDaniil <ParubovDaniil@mail.ru>
2024-03-20 21:23:43 +0500

    lab 5

--- mod5/task1.py
+++ mod5/task1.py
@@ -1,35 +1,24 @@
-class Node:
-    # Вспомогательный класс для узлов списка
-    def init(self, data):
-        self.data = data  # храним информацию
-        self.pref = None  # ссылка на предыдущий узел
+class Car:
+    def __init__(self, brand, model, year, color):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.color = color
 
-class Stack:
-    # Основной класс для стека
-    def init(self):
-        self.end = None  # ссылка на конец стека
+    def display_info(self):
+        print(f"Brand: {self.brand}")
+        print(f"Model: {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Color: {self.color}")
 
-    def pop(self):
-        # возвращение последнего элемента из списка с удалением его из списка
-        if self.end is None:
-            return None
-        else:
-            val = self.end.data
-            self.end = self.end.pref
-            return val
+    def honk(self):
+        print("Beep Beep!")
+# Пример использования
+car1 = Car("Toyota", "Corolla", 2020, "Silver")
+car2 = Car("Honda", "Civic", 2018, "Red")
 
-    def push(self, val):
-        # добавление элемента val в конец списка
-        new_node = Node(val)
-        if self.end is None:
-            self.end = new_node
-        else:
-            new_node.pref = self.end
-            self.end = new_node
+car1.display_info()
+car1.honk()
 
-    def print(self):
-        # вывод на печать содержимого стека
-        current = self.end
-        while current:
-            print(current.data)
-            current = current.pref
+car2.display_info()
+car2.honk()
--- mod5/task2.py
+++ mod5/task2.py
@@ -1,33 +1,38 @@
-class Queue:
-    """
-    Основной класс
-    """
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-    def init(self):
-        self.start = None  # ссылка на начало очереди
-        self.end = None  # ссылка на конец очереди
+    def greet(self):
+        if self.gender == 'male':
+            print("Hello, Mr. " + self.name)
+        elif self.gender == 'female':
+            print("Hello, Ms. " + self.name)
+        else:
+            print("Hello, " + self.name)
 
-    def pop(self):
-        """
-        возвращаем элемент val из начала списка с удалением из списка
-        """
-        pass
-        return val
+    def get_age_group(self):
+        if self.age < 18:
+            return "Child"
+        elif self.age < 60:
+            return "Adult"
+        else:
+            return "Senior Citizen"
+# Пример использования
+person1 = Person("Alice", 30, "female")
+person2 = Person("Bob", 25, "male")
+person3 = Person("Chris", 18, "male")
+person4 = Person("Diana", 10, "female")
+person5 = Person("Eve", 50, "female")
+person6 = Person("Frank", 35, "male")
+person7 = Person("Grace", 68, "female")
+person8 = Person("Harry", 55, "male")
+person9 = Person("Ivy", 15, "female")
 
-    def push(self, val):
-        """
-        добавление элемента val в конец списка
-        """
-        pass
+person_list = [person1, person2, person3, person4, person5, person6, person7, person8, person9]
 
-    def insert(self, n, val):
-        """
-        вставить элемент val между элементами с номерами n-1 и n
-        """
-        pass
-
-    def print(self):
-        """
-        вывод на печать содержимого очереди
-        """
-        pass
+for person in person_list:
+    person.greet()
+    print(person.get_age_group())
+    print()
